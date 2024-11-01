7043736c49085a76754f124d35fcb0353a2cc886
Jason Scurtu <code@xarbit.com>
2024-11-01 13:53:17 +0100

    Initial commit

---
+++ lua/plugins/core.lua
@@ -0,0 +1,69 @@
+return {
+
+  -- set the default theme to be used.
+  -- See: https://www.lazyvim.org/configuration#icons--colorscheme
+  {
+    'LazyVim/LazyVim',
+    opts = {
+      colorscheme = 'catppuccin-latte',
+    },
+  },
+
+  -- configure neo-tree, we do want to see dotfiles and co.
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = false,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_by_name = {
+            '.git',
+            '.DS_Store',
+            'thumbs.db',
+          },
+          never_show = {
+            '.git',
+            '.DS_Store',
+            'thumbs.db',
+          },
+        },
+      },
+    },
+  },
+  -- configure treesitter
+  {
+    'nvim-treesitter/nvim-treesitter',
+    opts = {
+      ensure_installed = {
+        'helm',
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'javascript',
+        'jsdoc',
+        'json',
+        'jsonc',
+        'lua',
+        'luadoc',
+        'luap',
+        'markdown',
+        'markdown_inline',
+        'printf',
+        'python',
+        'query',
+        'regex',
+        'toml',
+        'tsx',
+        'typescript',
+        'vim',
+        'vimdoc',
+        'xml',
+        'yaml',
+      },
+    },
+  },
+}
