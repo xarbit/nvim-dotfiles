e4867e76ba6d82217b0c9198cac2bb892b472da0
Jason Scurtu <code@xarbit.com>
2024-11-01 20:57:13 +0100

    add toggleterm

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,13 @@
+return {
+  -- use toggleterm
+  {
+    -- {'akinsho/toggleterm.nvim', version = "*", config = true}
+    -- or
+    {
+      'akinsho/toggleterm.nvim',
+      version = '*',
+      opts = {--[[ things you want to change go here]]
+      },
+    },
+  },
+}
